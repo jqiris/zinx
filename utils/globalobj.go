4384b00aebea11da314b589bf2fe623bcf134baf
jmesyan <jieqiang1017@gmail.com>
2021-04-23 16:30:10 +0800

    server id

--- utils/globalobj.go
+++ utils/globalobj.go
@@ -22,7 +22,7 @@ type GlobalObj struct {
 		Server
 	*/
 	// TCPServer ziface.IServer //当前Zinx的全局Server对象
-	ServerId string `yaml:"server_id"` //当前服务器ID
+	ServerId int    `yaml:"server_id"` //当前服务器ID
 	Host     string `yaml:"host"`      //当前服务器主机IP
 	TCPPort  int    `yaml:"tcp_port"`  //当前服务器主机监听端口号
 	Name     string `yaml:"name"`      //当前服务器名称
